db17bb680880a5d1278416172f29870f371f6254
Dimitris Damilos <d.k.damilos@gmail.com>
2024-07-15 13:44:10 +0100

    Fix copy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => (
   <header className="w-full flex pt-6 pb-4 px-4 gap-x-3 items-center">
     <img className="h-[54px] w-auto" src="img/crest.svg" />
-    <h1 className="text-base leading-[18px] max-w-[112px]">Greece national teams games</h1>
+    <h1 className="text-base leading-[18px] max-w-[112px]">Greek national teams’ games</h1>
   </header>
 );
 
